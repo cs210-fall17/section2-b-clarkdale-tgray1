1bd96ca8c062b57f0ac8592f35acc3025c106016
Tyler Evan Thomas Gray <tgray1@harvard.cs.arizona.edu>
2017-09-11 09:42:32 -0700

    tgray1 first commit

--- Hello.java
+++ Hello.java
@@ -10,7 +10,7 @@ public class Hello {
 
     public static void callSign() {
         
-        // Red Leader should add a print statement that says "Lock S-foils in attack position." 
+        System.out.println("Lock S-foils in attack position."); 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
     }
@@ -24,7 +24,7 @@ public class Hello {
 
     public static void redLeader() {
  
-        // Red Leader should add a print statement that says "I copy, gold leader!"
+        System.out.println("I copy, gold leader!");
         
 
     }
